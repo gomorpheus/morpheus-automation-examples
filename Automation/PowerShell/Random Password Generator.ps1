e9b53d67ad80d7ebbb32213cb78aaac298632e39
Chris Bunge <cbunge@morpheusdata.com>
2020-12-21 16:46:26 -0500

    still uploading

---
+++ Automation/PowerShell/Random Password Generator.ps1
@@ -0,0 +1,25 @@
+﻿function Get-RandomCharacters($length, $characters)
+{
+    $random = 1 .. $length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs = ""
+    return [String]$characters[$random]
+}
+ 
+function Scramble-String([string]$inputString)
+{
+    $characterArray = $inputString.ToCharArray()
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length
+    $outputString = -join $scrambledStringArray
+    return $outputString
+}
+# Generate a random password
+# Usage: random-password 
+Function random-password {
+    $password = Get-RandomCharacters -length 8 -characters 'abcdefghiklmnoprstuvwxyz'
+    $password += Get-RandomCharacters -length 8 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+    $password += Get-RandomCharacters -length 4 -characters '1234567890'
+    $password += Get-RandomCharacters -length 3 -characters '!%/()=}][{@#+'
+ 
+    $password = Scramble-String $password
+    return $password
+}
