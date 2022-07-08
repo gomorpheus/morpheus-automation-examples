faddde9f73c2c943463c524cbc945e4eac9b93e8
Anish <cuxtud@gmail.com>
2022-07-08 17:01:54 +0100

    Added some info messages

--- Python/Service Plans/vmConverttoManaged.py
+++ Python/Service Plans/vmConverttoManaged.py
@@ -15,8 +15,9 @@ def checkVMPlan(vmId):
     print(f"VM { vName } is assigned plan { planName }.\n")
 
 
-def convertToManaged(planId,vmName,vmId, planName):
-    print (f'Plan ID: {planId}, VM Name: {vmName}, VM ID: {vmId}. \n')
+def convertToManaged(planId,vmName,vmId, planName,vmPlan):
+    print(f"The current VM Plan is: {vmPlan}. ")
+    print (f'The VM will be converted to managed with these values. \nPlan Name: {planName}, VM Name: {vmName}, VM ID: {vmId}. \n')
     url = f'https://{ host }/api/servers/{ vmId }/make-managed'
     jbody={"server": {"plan": {"id": planId}}, "instllAgent": False}
     body=json.dumps(jbody)
@@ -32,14 +33,14 @@ def convertToManaged(planId,vmName,vmId, planName):
 
 
 
-def getServicePlan(priceSetId, vmName, zoneName, vmId):
+def getServicePlan(priceSetId, vmName, zoneName, vmId, vmPlan):
     print("Get Service Plan function")
     url = f'https://{ host }/api/service-plans?includeZones=true&provisionTypeId=17'
     r = requests.get(url, headers = headers, verify = False)
     data = r.json()
     l = len(data['servicePlans'])
-    servicePlanObject = json.dumps(data['servicePlans'], indent=2)
-    print(servicePlanObject)
+    #servicePlanObject = json.dumps(data['servicePlans'], indent=2)
+    #print(servicePlanObject)
     for i in range(0, l):
         p = len(data['servicePlans'][i]['priceSets'])
         for x in range(0, p):
@@ -47,9 +48,9 @@ def getServicePlan(priceSetId, vmName, zoneName, vmId):
                 print(f"Plan { data['servicePlans'][i]['name'] } should be assigned to vm { vmName } as it has a priceSet { data['servicePlans'][i]['priceSets'][x]['name'] } mapped to cloud { zoneName }.\n")
                 planId = data['servicePlans'][i]['id']
                 planName = data['servicePlans'][i]['name']
-                convertToManaged(planId, vmName, vmId, planName)
+                convertToManaged(planId, vmName, vmId, planName, vmPlan)
 
-def matchPriceSetforZoneId(zoneId, zoneName, vmName, vmId):
+def matchPriceSetforZoneId(zoneId, zoneName, vmName, vmId, vmPlan):
     url = f'https://{ host }/api/price-sets?includeZones=true&phrase=NA%20'
     r = requests.get(url, headers=headers, verify=False)
     data = r.json()
@@ -62,7 +63,7 @@ def matchPriceSetforZoneId(zoneId, zoneName, vmName, vmId):
                 if zoneId == data['priceSets'][i]['zone']['id']:
                     print(f"Price set { data['priceSets'][i]['name'] } is mapped to cloud { zoneName } for vm { vmName }. Now we need to get the right plan for vm { vmName }.\n " )
                     priceSetId = data['priceSets'][i]['id']
-                    getServicePlan(priceSetId, vmName, zoneName, vmId)
+                    getServicePlan(priceSetId, vmName, zoneName, vmId, vmPlan)
             else:
                 print(f"Price set {data['priceSets'][i]['name']} does not have a cloud associated with it, not a dedicated price set.")
 
@@ -80,16 +81,18 @@ def getDiscoveredVM():
         for i in range(0, l):
             vmId = data['servers'][i]['id']
             print(f"VM ID: {vmId}")
+            vmPlan = data['servers'][i]['plan']['name']
+            print(f"Current VM Plan: {vmPlan}")
             vmName = data['servers'][i]['name']
             print(f"VM Name: {vmName}")
             vmZoneId = data['servers'][i]['zone']['id']
             print(f"VM Zoneid: {vmZoneId}")
             vmZoneName = data['servers'][i]['zone']['name']
             print(f"VM Zone Name: {vmZoneName}")
-            vmobject = json.dumps(data['servers'][i], indent=2)
-            print(vmobject)
+            #vmobject = json.dumps(data['servers'][i], indent=2)
+            #print(vmobject)
             # Search all price sets where the zoneId matches. This is to make sure that this discovered vm will be assigned a plan which is dedicated to its cloud.
-            matchPriceSetforZoneId(vmZoneId, vmZoneName, vmName, vmId)
+            matchPriceSetforZoneId(vmZoneId, vmZoneName, vmName, vmId,vmPlan)
 
 ### Main
 getDiscoveredVM()
