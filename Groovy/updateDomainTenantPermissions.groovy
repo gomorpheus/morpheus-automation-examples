db8f616464b1f61ce147c89c7bfb3ccabf80d8b4
Anish <cuxtud@gmail.com>
2022-10-13 14:07:51 +0100

    used typeahead input to create the sites list

--- Groovy/updateDomainTenantPermissions.groovy
+++ Groovy/updateDomainTenantPermissions.groovy
@@ -2,34 +2,39 @@ import com.morpheus.NetworkDomainService
 import com.morpheus.PermissionService
 import com.morpheus.NetworkDomain
 import com.bertramlabs.plugins.Account
-
+ 
 PermissionService permissionService
 NetworkDomain networkDomain
-// Get the object of the Network Domain
-def networkDomainName = NetworkDomain.findByName('test.morpehusdata.com')
+def networkDomainName = NetworkDomain.findByName('test.morpheusdata.com')
+println "Domain info"
 println networkDomainName.id
 println networkDomainName.name
-
-// Get the master tenant object
+ 
 def masterTenant = Account.where{'masterAccount' == true }.find()
+println "Master tenant info"
 println masterTenant.id
 println masterTenant.name
-
-// Get sub tenant object
-def subTenant = Account.findByName('')
+ 
+def subTenant = Account.findByName('abc')
+println "Subtenant info"
 println subTenant.id
 println subTenant.name
-
+ 
 def tenantIds = []
+println "Master tenant info"
 tenantIds << masterTenant.id
 tenantIds << subTenant.id
 println tenantIds
-
-// Initialize global application Context for permissionService
+ 
+def groupItems = customOptions.groupList
+def groupStrg = []
+groupStrg = groupItems.collect { ["id": it, "default": true] }
+ 
+ 
 permissionService = grails.util.Holders.applicationContext['permissionService']
 permissionService.updateTenantPermissions('NetworkDomain',networkDomainName.id, tenantIds.collect { it.toLong()} )
-
-// Update group permissions for the network domain
-def resourcePermissions = [all: false, sites: [[id: 70, default: true]] ]
+ 
+//def resourcePermissions = [all: false, sites: [[id: 70, default: true]] ]
+def resourcePermissions = [all: false, sites: groupStrg]
 permissionService.updateResourcePermissions(masterTenant, 'NetworkDomain', networkDomainName.id, resourcePermissions)
 results.success
