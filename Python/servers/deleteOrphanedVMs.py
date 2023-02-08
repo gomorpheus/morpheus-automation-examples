655c786f5aa43816d820d59438d6d3d47dbb28dd
Anish <cuxtud@gmail.com>
2023-02-08 13:36:19 +0000

    updated server type

--- Python/servers/deleteOrphanedVMs.py
+++ Python/servers/deleteOrphanedVMs.py
@@ -1,14 +1,14 @@
 #The script will find all discovered vm's which are in the power state off
 # It would then delete the vm from morpheus but not from hypervisor. It is just deleting the vm record from morpheus 
 import requests
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 host=morpheus['morpheus']['applianceHost']
 token=morpheus['morpheus']['apiAccessToken']
 headers = {"Content-Type":"application/json","Accept":"application/json","Authorization": "BEARER " + (token)}
 
 def getListofDiscoveredVM():
-    url = ("https://%s/api/servers?serverType=vmware&powerState=off&max=1&offset=0") % (host)
+    url = ("https://%s/api/servers?serverType=VMware+VM&powerState=off&max=1&offset=0") % (host)
     r = requests.get(url, headers=headers, verify=False)
     data = r.json()
     serverName = data['servers'][0]['name']
