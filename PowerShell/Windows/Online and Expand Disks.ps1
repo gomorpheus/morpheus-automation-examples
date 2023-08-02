524213b109ed9105b53a26f9f266b7a402df26ca
cbunge <cbunge@morpheusdata.com>
2023-08-02 09:35:43 -0400

    updating this code a bit

--- PowerShell/Windows/Online and Expand Disks.ps1
+++ PowerShell/Windows/Online and Expand Disks.ps1
@@ -9,6 +9,8 @@
         Set this in the Pre-provision and Reconfigure phases of your provisioning Workflow.
         There are additional loops in this to name new drives based on their label in Morpheus. This can be removed easily if not desired.
 
+        Additionally, if you set the code value of your layout to 'sql' it will set properly sized 64k partitions for your additional disks.
+
     .EXAMPLE
         1. PowerShell task with execute target as 'Resource'
         2. Set Task as Pre-provision and Reconfigure Phase of Provisioning WorkFlow    
@@ -23,19 +25,24 @@ Get-Partition | Where-Object -FilterScript {$_.Type -eq 'Recovery'} | Remove-Par
 #Variables
 $disks = get-disk
 $size = Get-PartitionSupportedSize -DriveLetter C
-$MorphOrder = "<%=server.volumes.displayOrder%>"
-$MorphName = "<%=server.volumes.name%>"
-$diskorder = $MorphOrder.Replace("[","").Replace("]","").Split(",").TrimStart(" ")
-$diskname = $MorphName.Replace("[","").Replace("]","").Split(",").TrimStart(" ")
+$diskorder = '<%= server.volumes.displayOrder.encodeAsJson().toString() %>' | ConvertFrom-Json
+$diskname = '<%= server.volumes.name.encodeAsJson().toString() %>' | ConvertFrom-Json
 $diskarray = $diskorder | Select-Object @{n='ID'; e={$diskorder[$_]}}, @{n='Name'; e={$diskname[$_]}}
+$layout = "<%= instance.layoutCode %>"
 
 
 foreach ($disk in $disks) {
     #Online, Initialize, Format, and Assign Drive Letter to additional drives
     IF ($disk.IsOffline -eq $true) {
-        Initialize-Disk -Number $disk.DiskNumber -PassThru|
-        New-Partition -UseMaximumSize -AssignDriveLetter|
-        Format-Volume -NewFileSystemLabel ($diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name) -FileSystem NTFS -confirm:$false
+        if ($layout -eq 'sql') {
+            Initialize-Disk -Number $disk.DiskNumber -PassThru|
+            New-Partition -UseMaximumSize -AssignDriveLetter|
+            Format-Volume -NewFileSystemLabel ($diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name) -FileSystem NTFS -AllocationUnitSize 65536 -confirm:$false
+        } else {
+            Initialize-Disk -Number $disk.DiskNumber -PassThru|
+            New-Partition -UseMaximumSize -AssignDriveLetter|
+            Format-Volume -NewFileSystemLabel ($diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name) -FileSystem NTFS -confirm:$false
+        }
     }
     #Confirm disk only has 1 partition with a drive letter, expand to max size.  Skip Multiple Drive Letters
     ELSE {
