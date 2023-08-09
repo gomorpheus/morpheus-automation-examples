a4249bfad3c85ef5339c6c3614075282d69e090e
Anish <cuxtud@gmail.com>
2023-08-09 22:47:40 +0530

    added vsphere provider version 0.14.5

--- Terraform/VMware/createFolder/provider.tf
+++ Terraform/VMware/createFolder/provider.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = ">= 0.14.5"
+    }
+  }
+}
 provider "vsphere" {
   user           = var.vc_user
   password       = var.vc_pass
   vsphere_server = var.vc_server
-
   allow_unverified_ssl = true
 }
