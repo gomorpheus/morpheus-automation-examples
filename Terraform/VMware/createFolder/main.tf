eb4533803b31e26b25887c5f39c886317f187d78
Anish <cuxtud@gmail.com>
2023-08-09 19:32:11 +0530

    changing the source to work with spec templates

--- Terraform/VMware/createFolder/main.tf
+++ Terraform/VMware/createFolder/main.tf
@@ -1,4 +1,5 @@
 module "createFolder" {
-   source = "../../modules/VMware/createFolder"
+   #source = "../../modules/VMware/createFolder"
+   source = "https://github.com/gomorpheus/morpheus-automation-examples/tree/main/Terraform/modules/VMware/createFolder"
    folder_name = var.folder_name
 }
