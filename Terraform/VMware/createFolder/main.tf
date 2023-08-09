58de5ed0996ecfdaffcd19ff12a96238319bc3d0
Anish <cuxtud@gmail.com>
2023-08-09 22:41:16 +0530

    updated source url

--- Terraform/VMware/createFolder/main.tf
+++ Terraform/VMware/createFolder/main.tf
@@ -1,5 +1,5 @@
 module "createFolder" {
    #source = "../../modules/VMware/createFolder"
-   source = "https://github.com/gomorpheus/morpheus-automation-examples/tree/main/Terraform/modules/VMware/createFolder"
+   source = "git::https://github.com/gomorpheus/morpheus-automation-examples/tree/main/Terraform/modules/VMware/createFolder"
    folder_name = var.folder_name
 }
