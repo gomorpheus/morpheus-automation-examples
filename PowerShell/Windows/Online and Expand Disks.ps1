59888dc9a6198e48ada5b121fb80d413d131eec2
Chris Bunge <BungeBash@gmail.com>
2024-04-18 17:30:05 -0400

    updating disk resize for cleaner output

--- PowerShell/Windows/Online and Expand Disks.ps1
+++ PowerShell/Windows/Online and Expand Disks.ps1
@@ -1,58 +1,75 @@
 <#
     .SYNOPSIS
-        This script automatically onlines and expands all drives found by the OS
+        This script automatically onlines and expands all drives found by the OS.
     
     .DESCRIPTION
         This script automatically onlines and expands all drives found by the OS.
         Note: This does not expand if you have multiple partitions on the same Dynamic root disk as a precaution.
         
-        Set this in the Pre-provision and Reconfigure phases of your provisioning Workflow.
+        Add Task to the (Pre/Post)Provisioning and Reconfigure phases of your Windows Provisioning Workflow.
+
         There are additional loops in this to name new drives based on their label in Morpheus. This can be removed easily if not desired.
 
         Additionally, if you set the code value of your layout to 'sql' it will set properly sized 64k partitions for your additional disks.
 
     .EXAMPLE
-        1. PowerShell task with execute target as 'Resource'
-        2. Set Task as Pre-provision and Reconfigure Phase of Provisioning WorkFlow    
+        1. PowerShell task with execute target as 'Resource'.
+        2. Add Task to the (Pre/Post)Provisioning and Reconfigure Phases of the Windows Provisioning WorkFlow.
 #>
 
 # Silence Output
 $ProgressPreference = 'SilentlyContinue'
 
 # Remove Recovery Partition (Optional)
+Write-Host "Checking Recovery Partition Status..." -ForegroundColor Cyan
 Get-Partition | Where-Object -FilterScript {$_.Type -eq 'Recovery'} | Remove-Partition -confirm:$false
+Write-Host "Complete!" -ForegroundColor Green
 
-#Variables
-$disks = get-disk
+# Variables
+$disks = Get-Disk
 $size = Get-PartitionSupportedSize -DriveLetter C
 $diskorder = '<%= server.volumes.displayOrder.encodeAsJson().toString() %>' | ConvertFrom-Json
 $diskname = '<%= server.volumes.name.encodeAsJson().toString() %>' | ConvertFrom-Json
 $diskarray = $diskorder | Select-Object @{n='ID'; e={$diskorder[$_]}}, @{n='Name'; e={$diskname[$_]}}
-$layout = "<%= instance.layoutCode %>"
+$layout = "<%= instance?.layoutCode %>"
 
 
 foreach ($disk in $disks) {
-    #Online, Initialize, Format, and Assign Drive Letter to additional drives
-    IF ($disk.IsOffline -eq $true) {
-        if ($layout -eq 'sql') {
-            Initialize-Disk -Number $disk.DiskNumber -PassThru|
-            New-Partition -UseMaximumSize -AssignDriveLetter|
-            Format-Volume -NewFileSystemLabel ($diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name) -FileSystem NTFS -AllocationUnitSize 65536 -confirm:$false
-        } else {
-            Initialize-Disk -Number $disk.DiskNumber -PassThru|
-            New-Partition -UseMaximumSize -AssignDriveLetter|
-            Format-Volume -NewFileSystemLabel ($diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name) -FileSystem NTFS -confirm:$false
-        }
-    }
-    #Confirm disk only has 1 partition with a drive letter, expand to max size.  Skip Multiple Drive Letters
-    ELSE {
-        $partition = $disk | Get-Partition | where DriveLetter -ne "`0"
+    $label = $diskarray | where {$_.id -eq $disk.number} | select -ExpandProperty name
+    Write-Host "Checking Disk `"${label}`"..." -ForegroundColor Cyan
 
-        if (($partition | measure).count -eq 1) {
-            $maxSize = ($partition | Get-PartitionSupportedSize).sizeMax
-            Resize-Partition -DriveLetter $partition.DriveLetter -Size $maxSize -ErrorAction SilentlyContinue
+    # Online, Initialize, Format, and Assign Drive Letter to additional drives
+    try {    
+        if ($disk.IsOffline -eq $true) {
+            if ($layout -eq 'sql') {
+                Write-Host "Initializing SQL Disk `"${label}`"..." -ForegroundColor White
+                Initialize-Disk -Number $disk.DiskNumber -PassThru|
+                New-Partition -UseMaximumSize -AssignDriveLetter|
+                Format-Volume -NewFileSystemLabel $label -FileSystem NTFS -AllocationUnitSize 65536 -confirm:$false | Out-Null
+            } else {
+                Write-Host "Initializing Disk `"${label}`"..." -ForegroundColor White
+                Initialize-Disk -Number $disk.DiskNumber -PassThru|
+                New-Partition -UseMaximumSize -AssignDriveLetter|
+                Format-Volume -NewFileSystemLabel $label -FileSystem NTFS -confirm:$false | Out-Null
             }
-        
+        } else {
+            # Confirm disk only has 1 partition with a drive letter, expand to max size, otherwise skip.
+            $partition = $disk | Get-Partition | where DriveLetter -ne "`0"
+    
+            if (($partition | measure).count -eq 1) {
+                $maxSize = ($partition | Get-PartitionSupportedSize).sizeMax
+                $ms = [math]::round($maxSize/1GB, 2)
+                if ($maxSize -ne $partition.Size) {
+                    Write-Host "Resizing Disk `"${label}`" to ${ms}GB..." -ForegroundColor White
+                    Resize-Partition -DriveLetter $partition.DriveLetter -Size $maxSize -ErrorAction SilentlyContinue
+                }
+            }
+        }
+    } catch {
+        Write-Host "An Error Occurred for Disk `"${label}`":" -ForegroundColor Red
+        Write-Host $_
+    } finally {
+        Write-Host "Disk `"${label}`" Complete!" -ForegroundColor Green
     }
 }
     
